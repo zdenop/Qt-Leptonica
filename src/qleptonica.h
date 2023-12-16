2a31cd62adf19c8fd7b8b8da0804af46d3dfdbce
zdenop <zdenop@gmail.com>
2023-12-16 21:51:49 +0100

    support leptonica >1.82

--- src/qleptonica.h
+++ src/qleptonica.h
@@ -5,6 +5,9 @@
 #include <QString>
 
 #include <leptonica/allheaders.h>
+#if (LIBLEPT_MAJOR_VERSION > 1) || (LIBLEPT_MINOR_VERSION > 82)
+    #include <leptonica/pix_internal.h>
+#endif
 
 QMap<int, QString> leptonicaFormats();
 QString getFormatDesc(int format);
