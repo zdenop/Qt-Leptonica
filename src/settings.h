1933e6cdf8268885b5f4fe66102a18a1744bb9cf
zdenop <zdenop@gmail.com>
2023-12-19 22:44:56 +0100

    fix version

--- src/settings.h
+++ src/settings.h
@@ -5,6 +5,6 @@
 #define SETTING_APPLICATION "Qt Leptonica"
 #define PROJECT_URL "https://github.com/zdenop/QT-Leptonica"
 #define PROJECT_URL_NAME "github.com/zdenop/QT-Leptonica"
-#define VERSION "1.0-alpha"
+#define VERSION "0.3"
 #endif // SETTINGS
 
